b3cc471f6cd09c76adddd4d2cc0bd9b36ce89963
jimmy <hustzch@163.com>
2022-01-26 22:48:49 +0800

    111

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
+import './axios.js' // 请求拦截
+// 全局注册
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+// use
+Vue.use(mavonEditor)
+
+Vue.prototype.$axios = axios
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
